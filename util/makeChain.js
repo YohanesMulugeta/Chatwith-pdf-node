3cb75e3f0d82b7a1256ff0e5dadc3189a633a09e
Yohanes <yohanesmulugeta21@gmail.com>
2023-05-19 01:03:22 +0300

    some css upgrade to the pricing card.

--- util/makeChain.js
+++ util/makeChain.js
@@ -1,7 +1,7 @@
 const { OpenAI } = require('langchain/llms/openai');
 const { ConversationalRetrievalQAChain } = require('langchain/chains');
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. if the follow up question is unrelated to the conversation make the standalone question to be same as the follow up question.
 
 Chat History:
 {chat_history}
@@ -19,7 +19,7 @@ Standalone question:`;
 // Helpful answer in markdown:`;
 
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of document to answer the question at the end.
-The qustion may be to ask you to generate a social media post based on the document, In that case generate a sample social media post
+The qustion may be to ask you to generate a social media or blog post based on the document, In that case generate a sample social media or blog post
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the document or based on the document, politely respond that you are tuned to only answer questions that are related to the document provided and generate what the document is about.
 
