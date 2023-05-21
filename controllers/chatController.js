d0b736aff3cc91a21c2914a3ab063b0ea185cc73
Yohanes <yohanesmulugeta21@gmail.com>
2023-05-21 16:27:00 +0300

    update token into jwt only in the websocket handler backend.

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -114,6 +114,7 @@ async function checkLoginStatus(req) {
   // ------------------- Check if user is logged in or not
   const token = req.cookies.jwt;
 
+  console.log('pussy');
   console.log(token);
 
   if (token === undefined)
