a7678dd636a3041652e6b31986a416b69006079a
Yohanes <yohanesmulugeta21@gmail.com>
2023-05-21 15:23:39 +0300

    handle errors grace fully on the websocket back end and front end.

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -11,38 +11,7 @@ const AppError = require('../util/AppError');
 // ------------------------ WEBSOCKET Chat
 exports.chat = async (ws, req) => {
   try {
-    console.log('connect try');
-    const { authorization } = req.headers;
-    const token =
-      (authorization?.startsWith('Bearer') && authorization.split(' ')[1]) ||
-      req.cookies.jwt;
-
-    if (!token)
-      throw new AppError(
-        'You are not loged in. Please login or register and try again.',
-        401
-      );
-    const { id, iat } = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-    const user = await User.findById(id).select('+password');
-
-    if (!user)
-      throw new AppError(
-        'There is no user with this token. Pleaase login and try again.',
-        400
-      );
-
-    if (user.isPassChangedAfter(iat))
-      throw new AppError(
-        'You have changed password recently. Please login again to get access.',
-        401
-      );
-
-    if (
-      user.subscriptionUpdatedAt.getTime() / 1000 <=
-      (Date.now() - 30 * 24 * 60 * 60 * 1000) / 1000
-    ) {
-      req.user = await user.resetUser();
-    }
+    const user = await checkLoginStatus(req);
 
     const { chatId } = req.params;
     const { vectorName: nameSpace, indexName } = await user.chats.id(chatId);
@@ -70,17 +39,12 @@ exports.chat = async (ws, req) => {
       try {
         const userN = await User.findById(user._id).select('+chats.chatHistory');
 
-        if (!userN.subscription)
-          throw new AppError('Please Subscribe to one of our plans to get going.', 400);
-
-        if (userN.conversationTokens < question.length / 4)
-          throw new AppError(
-            'You have finished your conversational tokens, Please upgrade to continue',
-            400
-          );
+        // Check users subscription status
+        await checkSubscriptionStat(userN, question);
 
         const chatHistory = userN.chats.id(chatId).chatHistory.slice(-5);
 
+        // Cut the creation of new question from reaching to the user
         let space = 0;
         const streamHandler = {
           handleLLMNewToken(token) {
@@ -91,13 +55,12 @@ exports.chat = async (ws, req) => {
               return;
             }
 
-            // console.log(token);
-
             ws.send(JSON.stringify({ data: token, event: 'data' }));
           },
         };
 
         const chain = makeChain(vectorStore, streamHandler);
+
         //Ask a question using chat history
         const sanitizedQuestion = question.replace('/n', ' ').trim();
 
@@ -107,17 +70,21 @@ exports.chat = async (ws, req) => {
           chat_history: chatHistory,
         });
 
+        // ---------- Sending the source with source event
         ws.send(JSON.stringify({ source: response.sourceDocuments, event: 'source' }));
 
         await userN.updateConversationTokens(
           (response.text.length + sanitizedQuestion.length) / 4
         );
 
+        // Updating users chat history
         userN.chats
           .id(chatId)
           .chatHistory.push([`Question: ${question}`, `Answer: ${response.text}`]);
+
         userN.updateChatModifiedDate(chatId);
       } catch (err) {
+        // ------ Handle Errors
         ws.send(
           JSON.stringify({
             event: 'error',
@@ -137,3 +104,57 @@ exports.chat = async (ws, req) => {
     );
   }
 };
+
+// /////////// //
+//    HELPERS //
+// ////////// //
+
+// Checks login Status
+async function checkLoginStatus(req) {
+  // ------------------- Check if user is logged in or not
+  const { authorization } = req.headers;
+  const token =
+    (authorization?.startsWith('Bearer') && authorization.split(' ')[1]) ||
+    req.cookies.jwt;
+
+  if (!token)
+    throw new AppError(
+      'You are not loged in. Please login or register and try again.',
+      401
+    );
+  const { id, iat } = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const user = await User.findById(id).select('+password');
+
+  if (!user)
+    throw new AppError(
+      'There is no user with this token. Pleaase login and try again.',
+      400
+    );
+
+  if (user.isPassChangedAfter(iat))
+    throw new AppError(
+      'You have changed password recently. Please login again to get access.',
+      401
+    );
+
+  if (
+    user.subscriptionUpdatedAt.getTime() / 1000 <=
+    (Date.now() - 30 * 24 * 60 * 60 * 1000) / 1000
+  ) {
+    user = await user.resetUser();
+  }
+
+  return user;
+}
+
+// ------------- Check token and subscription stat
+async function checkSubscriptionStat(user, question) {
+  if (!user.subscription)
+    throw new AppError('Please Subscribe to one of our plans to get going.', 400);
+
+  if (user.conversationTokens < question.length / 4)
+    throw new AppError(
+      'You have finished your conversational tokens, Please upgrade to continue',
+      400
+    );
+}
